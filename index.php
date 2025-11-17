2da53c64f7f506541f9f4b1971591b437bd53015
Zen <zenmuhammad072@gmail.com>
2025-11-17 08:14:03 +0700

    persiapan

--- index.php
+++ index.php
@@ -3,9 +3,41 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Zen Tutorial</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+
 </head>
 <body>
+    <div class="container">
+        <div class="header">
+            <span class="judul">Sistem Informasi Percetakan Berjaya Selamanya</span>
+            <div class="hero-container">
+                <div class="hero-item">
+                     Kami menyediakan layanan percetakan lengkap, termasuk print warna, fotokopi, penjilidan dokumen, serta pembuatan spanduk, banner, dan media promosi lainnya secara cepat dan berkualitas.
+                </div>
+                <div class="hero-item" >Kontak Person : <i class="fa fa-phone"></i> 081211111111</div>
+                <div class="hero-item">Alamat : <i class="fa fa-map-pin"></i> Jalan Utama 2 Kampung Kolam</div>
+            </div>
+        </div>
+            <div class="leftSide">
+                <ul class="menu">
+                    <li class="menu-item"> <i class="fa fa-home"></i> Home</li>
+                    <li class="menu-item"><i class="fa fa-list-alt"></i> Barang</li>
+                    <li class="menu-item"> <i class="fa fa-credit-card"></i> Transaksi</li>
+                </ul>
+            </div>
+            <div class="konten">
+                <div class="isi-konten">
+                    Selamat Datang di Sistem Informasi Percetakan Berjaya Selamanya. Mohon kelola semua transaksi menggunakan sistem ini.
+                    <h5>Grafik Transaksi</h5>
+                </div>
+            </div>
+            <div class="rightSide">
+                <div class="slogan">Mengelola Percetakan Lebih Cepat, Lebih Tepat.</div>
+            </div>
+        <div class="footer">&copy; Zen Tutorial 2025</div>
+    </div>
     
 </body>
 </html>
