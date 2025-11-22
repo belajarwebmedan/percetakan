3b7e3c8a2084b86f6a2b5d6c2b1d4cde80fe7ce6
Zen <zenmuhammad072@gmail.com>
2025-11-22 17:21:26 +0700

    penambahan rancangan Database 2

--- db_percetakan.sql
+++ db_percetakan.sql
@@ -11,28 +11,174 @@
  Target Server Version : 100432 (10.4.32-MariaDB)
  File Encoding         : 65001
 
- Date: 17/11/2025 09:31:55
+ Date: 22/11/2025 17:21:10
 */
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
 -- ----------------------------
--- Table structure for tblbarang
+-- Table structure for barang
 -- ----------------------------
-DROP TABLE IF EXISTS `tblbarang`;
-CREATE TABLE `tblbarang`  (
-  `idBarang` int NOT NULL AUTO_INCREMENT,
-  `namaBrg` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
-  `satuan` enum('PCS','BOTOL','KOTAK') CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+DROP TABLE IF EXISTS `barang`;
+CREATE TABLE `barang`  (
+  `id_barang` int NOT NULL AUTO_INCREMENT,
+  `nama_barang` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `satuan` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `stok` int NULL DEFAULT NULL,
   `harga` decimal(10, 2) NULL DEFAULT NULL,
-  PRIMARY KEY (`idBarang`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 7 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+  PRIMARY KEY (`id_barang`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 11 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
--- Records of tblbarang
+-- Records of barang
 -- ----------------------------
-INSERT INTO `tblbarang` VALUES (2, 'pencil', 'PCS', 5000.00);
-INSERT INTO `tblbarang` VALUES (4, 'pencil', 'PCS', 5000.00);
+INSERT INTO `barang` VALUES (8, 'alert(&quot;halo&quot;)', 'PCS', 5000, NULL);
+INSERT INTO `barang` VALUES (9, '<script>alert(\"halo\")</script>', 'PCS', 5000, NULL);
+INSERT INTO `barang` VALUES (10, '&#039;satuan', 'PCS', 5000, NULL);
+
+-- ----------------------------
+-- Table structure for kategori_layanan
+-- ----------------------------
+DROP TABLE IF EXISTS `kategori_layanan`;
+CREATE TABLE `kategori_layanan`  (
+  `id_kategori` int NOT NULL AUTO_INCREMENT,
+  `nama_kategori` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id_kategori`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of kategori_layanan
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for layanan
+-- ----------------------------
+DROP TABLE IF EXISTS `layanan`;
+CREATE TABLE `layanan`  (
+  `id_layanan` int NOT NULL AUTO_INCREMENT,
+  `id_kategori` int NULL DEFAULT NULL,
+  `nama_layanan` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `harga_dasar` decimal(10, 2) NULL DEFAULT NULL,
+  `satuan` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id_layanan`) USING BTREE,
+  INDEX `id_kategori`(`id_kategori` ASC) USING BTREE,
+  CONSTRAINT `layanan_ibfk_1` FOREIGN KEY (`id_kategori`) REFERENCES `kategori_layanan` (`id_kategori`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of layanan
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for log_aktifitas
+-- ----------------------------
+DROP TABLE IF EXISTS `log_aktifitas`;
+CREATE TABLE `log_aktifitas`  (
+  `id_log` int NOT NULL AUTO_INCREMENT,
+  `id_user` int NULL DEFAULT NULL,
+  `aktivitas` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL,
+  `waktu` datetime NULL DEFAULT NULL,
+  PRIMARY KEY (`id_log`) USING BTREE,
+  INDEX `id_user`(`id_user` ASC) USING BTREE,
+  CONSTRAINT `log_aktifitas_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `users` (`id_user`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of log_aktifitas
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for pelanggan
+-- ----------------------------
+DROP TABLE IF EXISTS `pelanggan`;
+CREATE TABLE `pelanggan`  (
+  `id_pelanggan` int NOT NULL,
+  `nama` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `telp` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `alamat` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL,
+  PRIMARY KEY (`id_pelanggan`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of pelanggan
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for pembayaran
+-- ----------------------------
+DROP TABLE IF EXISTS `pembayaran`;
+CREATE TABLE `pembayaran`  (
+  `id_bayar` int NULL DEFAULT NULL,
+  `id_pesanan` int NULL DEFAULT NULL,
+  `tanggal_bayar` datetime NULL DEFAULT NULL,
+  `jumlah_bayar` decimal(10, 2) NULL DEFAULT NULL,
+  `metode` enum('cash','transfer','ewallet') CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  INDEX `id_pesanan`(`id_pesanan` ASC) USING BTREE,
+  CONSTRAINT `pembayaran_ibfk_1` FOREIGN KEY (`id_pesanan`) REFERENCES `pesanan` (`id_pesanan`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of pembayaran
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for pesanan
+-- ----------------------------
+DROP TABLE IF EXISTS `pesanan`;
+CREATE TABLE `pesanan`  (
+  `id_pesanan` int NOT NULL AUTO_INCREMENT,
+  `kode_invoice` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `id_pelanggan` int NULL DEFAULT NULL,
+  `tanggal_pesanan` datetime NULL DEFAULT NULL,
+  `status` enum('pending','proses','selesai','diambil') CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `total` decimal(10, 2) NULL DEFAULT NULL,
+  `catatan` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL,
+  PRIMARY KEY (`id_pesanan`) USING BTREE,
+  INDEX `id_pelanggan`(`id_pelanggan` ASC) USING BTREE,
+  CONSTRAINT `pesanan_ibfk_1` FOREIGN KEY (`id_pelanggan`) REFERENCES `pelanggan` (`id_pelanggan`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of pesanan
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for pesanan_detail
+-- ----------------------------
+DROP TABLE IF EXISTS `pesanan_detail`;
+CREATE TABLE `pesanan_detail`  (
+  `id_detail` int NOT NULL AUTO_INCREMENT,
+  `id_pesanan` int NULL DEFAULT NULL,
+  `id_layanan` int NULL DEFAULT NULL,
+  `qty` int NULL DEFAULT NULL,
+  `harga_satuan` decimal(10, 2) NULL DEFAULT NULL,
+  `subtotal` decimal(10, 2) NULL DEFAULT NULL,
+  PRIMARY KEY (`id_detail`) USING BTREE,
+  INDEX `id_pesanan`(`id_pesanan` ASC) USING BTREE,
+  INDEX `id_layanan`(`id_layanan` ASC) USING BTREE,
+  CONSTRAINT `pesanan_detail_ibfk_1` FOREIGN KEY (`id_pesanan`) REFERENCES `pesanan` (`id_pesanan`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  CONSTRAINT `pesanan_detail_ibfk_2` FOREIGN KEY (`id_layanan`) REFERENCES `layanan` (`id_layanan`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of pesanan_detail
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for users
+-- ----------------------------
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users`  (
+  `id_user` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `role` enum('admin','kasir','operator') CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id_user`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of users
+-- ----------------------------
 
 SET FOREIGN_KEY_CHECKS = 1;
